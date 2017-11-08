9c5f0100ede40358c13e17825982058d2e988ad9
Lasha Tsintsabadze <lasha.tsintsabadze@gmail.com>
2017-11-08 17:19:52 +0200

    450 enhancement qr code (#508)
    * Release 1.3.2 (#493)
    
    * Transaction Details (#389)
    
    * Transaction Details
    
    * Transaction Details
    
    * MacOS Background (#391)
    
    * Sending form state (#410)
    
    1) “SEND” button is disabled as long as form is “dirty”
    2) Once inputs were filled with correct values “SEND” button becomes enabled
    3) “SEND” button is disabled during “sending” event
    4) Once “sending” event is completed and no errors were triggered form is cleared.
    
    * making travis green
    
    * Gruntfile changes for Linux
    
    * prepare-package changes for Linux
    
    * Gruntfile changes for Linux
    
    * prepare-package changes for Linux
    
    * Issue 311 (#444)
    
    * implemented send all && better message for connection problems
    
    * cleanup
    
    * #430 amoun validation
    
    * #311 fixed send all funds with funding hub
    
    * #430 fixed length validations
    
    * #438 fixed case when user had bytes and still chanegd from hub fee
    
    * making travis green
    
    * cleanup
    
    * selectable text in alert
    
    * fee type option cleanup (#443)
    
    * #434 made funding node and dicsovery service not removable from paired devices (#446)
    
    * Issue 288 (#442)
    
    * removed light wallet check in view
    
    * implementation of restore wallet from seed for light version
    
    * fixed styles
    
    * making travis green
    
    * android build fixes
    
    * #427 transaction fee is shown in bytes, it should be in dags
    
    * Send all imporovements (#449)
    
    * removed useless funding node notification interval
    
    * #311 added popup before sending all amount. && made amount changable
    
    * style fixes in seed
    
    * #311 more fixes to send all
    
    * fixed lint
    
    * issue 437 (#447)
    
    * #437 getting version from package for sentry
    
    * making travis green
    
    * Merge branch 'testnet' into issue-427
    
    Conflicts:
    	src/js/controllers/walletHome.js
    
    * Merge branch 'testnet' into issue-427
    
    Conflicts:
    	src/js/controllers/walletHome.js
    
    * #382 make password restriction
    
    * fix eslint errors
    
    * #461 Fix client after byteballcore update
    
    * add specific commit reference to environments
    
    * fix eslint
    
    * Providing a proof of something
    
    * Brand new Proofing Service
    
    * Sending requests and responses during a proofing sessions using the dagcoin protocol
    
    * Fixing text alignment inside wallet preferences.
    
    * Sending proofs of addresses whenever the customer wants to use the funding node.
    
    Linking generated addresses to the master address on the funding node
    
    * Working around eslint problems
    
    * Version update
    
    * Fixing on-the-run proofing process
    
    * Android apps can't easily read package.json
    
    * Updated testnet version
    
    * Forbidding to use the funding address with secondary wallets
    
    * Eslint complains on missing spaces between curly brakets and text
    
    * The proofing service can correctly reply to the question: "do you have enough dagcoins on spare addresses to start to funding process?"
    
    * Using the latest byteball core
    
    * The shared_address for funding purpose must be linked to the wallet master address.
    
    * ESlint strikes again
    
    * load-address gets a new parameter: the master address to be loaded.
    
    * #441 Provide correct error message in case of network errors (#459)
    
    * #441 Provide correct error message in case of network errors
    
    * #441 changed in progress payment notification with more description text
    
    * #472 : Backup functionality has problem on windows (#484)
    
    * #472 : Backup functionality has problem on windows
    
    * fix eslint
    
    * #472: Backup functionality has problem on windows
    
    * revert mistaken nwjs version
    
    * Create dagQrCode.directive.js
    
    * Create dagQrCode.library.js
    
    * Update walletHome.html
    
    * Add files via upload
    
    * New custom QR-code directive
    
    * small fixes

---
+++ src/js/directives/dagQrCode/dagQrCode.directive.js
@@ -0,0 +1,86 @@
+(() => {
+  'use strict';
+
+  const QRCode = require('qrcode');
+
+  /**
+   * @desc custome qr-code drawer
+   * @example <dag-qr-code></dag-qr-code>
+   */
+  angular
+    .module('copayApp.directives')
+    .directive('dagQrCode', dagQrCode);
+
+  dagQrCode.$inject = [];
+
+  function dagQrCode() {
+    return {
+      restrict: 'E',
+      scope: {},
+      link: ($scope, element, attrs) => {
+        function drawLogo(logoPath, canvas, callback) {
+          const img = new Image();
+          img.onload = () => {
+            const ctx = canvas.getContext('2d');
+            const x = (140 - img.width) / 2;
+            const y = (140 - img.height) / 2;
+            ctx.drawImage(img, x, y, img.width, img.height);
+            callback(null, canvas);
+          };
+          img.src = logoPath;
+        }
+
+        function changeColor(r, g, b, canvas) {
+          const ctx = canvas.getContext('2d');
+          const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+          for (let i = 0, ii = imgData.data.length; i < ii; i += 4) {
+            imgData.data[i] |= r;
+            imgData.data[i + 1] = g | imgData.data[i + 1];
+            imgData.data[i + 2] = b | imgData.data[i + 2];
+          }
+          ctx.putImageData(imgData, 0, 0);
+          return canvas;
+        }
+
+        function perform(text, options, nextStep) {
+          let canvas = document.createElement('canvas');
+
+          QRCode.toCanvas(canvas, text, {
+            errorCorrectionLevel: 'H'
+          }, (err) => {
+            if (err) {
+              nextStep(err, null);
+              return false;
+            }
+            if (options.r || options.g || options.b) {
+              canvas = changeColor(options.r, options.g, options.b, canvas);
+            }
+            if (options.logoPath) {
+              drawLogo(options.logoPath, canvas, nextStep);
+            } else {
+              nextStep(null, canvas);
+            }
+          });
+        }
+
+        attrs.$observe('url', (url) => {
+          if (url && url.length > 20) {
+            perform(url, {
+              r: 213,
+              g: 31,
+              b: 38,
+              logoPath: 'img/dagcoin_35x35.png'
+            }, (err, canvas) => {
+              if (err) {
+                alert(err);
+              } else {
+                const src = canvas.toDataURL();
+                element.html(`<img width="260" src="${src}">`);
+              }
+            });
+          }
+        });
+      }
+    };
+  }
+})();
