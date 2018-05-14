89215d035565257ff725165795d9572d07ad05c2
dkostenevich <dima.spaker@gmail.com>
2018-05-14 17:14:23 +0300

    added icon to header

--- app/home/home.js
+++ app/home/home.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 import {
-    StyleSheet
+    StyleSheet, Image, Platform
 } from 'react-native';
 
 import GeneralLayout from "../general/generalLayout";
@@ -10,6 +10,7 @@ import DagBottomMenu from "../controls/dagMenu/dagBottomMenu";
 import BackgroundLayout from "../general/backgroundLayout";
 import {container} from "../styles/main";
 import Receive from "./receive";
+import DagSimpleButton from "../controls/dagSimpleButton";
 
 class Home extends Component {
     constructor() {
@@ -45,10 +46,39 @@ class Home extends Component {
         }
     }
 
+    onShareButtonClick() {
+        console.log('share');
+    }
+
+    renderContactButton() {
+        switch (this.state.selectedItem) {
+            case 0:
+                return null;
+            case 1:
+                if (Platform.OS === "web"){
+                    return null;
+                }
+
+                return (
+                    <DagSimpleButton style={StyleSheet.flatten([container.p20])} onClick={this.onShareButtonClick.bind(this)}>
+                        <Image source={require('../../img/share-white.png')} style={styles.shareIcon} />
+                    </DagSimpleButton>
+                );
+            case 2:
+                return null;
+            case 3:
+                return null;
+        }
+    }
+
     render() {
         return (
             <GeneralLayout style={StyleSheet.flatten([styles.container])}>
-                <PageHeader color={'red'} hasMenu={true} title={'Wallet #1'.toUpperCase()}></PageHeader>
+                <PageHeader color={'red'}
+                            hasMenu={true}
+                            title={'Wallet #1'.toUpperCase()}
+                            renderCustomAction={this.renderContactButton.bind(this)}
+                />
                 <BackgroundLayout style={StyleSheet.flatten([styles.container, container.p0])}>
                     {this.renderContent()}
                     <DagBottomMenu
@@ -65,6 +95,10 @@ class Home extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    shareIcon: {
+        width: 20,
+        height: 20
     }
 });
 
