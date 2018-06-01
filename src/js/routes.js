d51bda7185d8048e70ffaaa811f0535f5a42f848
dkostenevich <dima.spaker@gmail.com>
2018-06-01 15:25:41 +0300

    added missed param

--- src/js/routes.js
+++ src/js/routes.js
@@ -632,7 +632,7 @@
       if (isCordova) {
         document.addEventListener('resume', () => {
           $timeout(() => {
-            configService.get((config) => {
+            configService.get((err, config) => {
               // password and finger print options are read from config and profile service
               const needPassword = !!profileService.profile.xPrivKeyEncrypted;
               const needFingerprint = !!config.touchId;
