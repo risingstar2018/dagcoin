252c5fccc24f8548a1048b9e4450e3d546ec42a0
msinansahin <msinansahi@gmail.com>
2018-06-05 13:20:40 +0300

    arrange testnetify run in both development and travis

--- devbuilds/testnetify.sh
+++ devbuilds/testnetify.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-sed -ie "s/version = '1.0'/version = '1.0t'/" ../node_modules/core/constants.js
+if [ -f ../node_modules/core/constants.js ]
+then
+  sed -ie "s/version = '1.0'/version = '1.0t'/" ../node_modules/core/constants.js
+else
+  sed -ie "s/version = '1.0'/version = '1.0t'/" ./node_modules/core/constants.js
+fi
